3426228b45749108084ad59b0c45e13c4008425c
okedo <eliahmwelangi01@gmail.com>
2023-09-06 13:48:29 +0300

    Javascript tutorial

---
+++ node.js
@@ -0,0 +1,84 @@
+// let age = 23;
+// console.log(age);
+
+// let year = 2023;
+// console.log(year);
+
+// year = 2025;
+// console.log(year);
+
+// const points = 100;
+// console.log(points)
+
+// var marks = 12;
+// console.log(marks)
+
+// String
+// let email = 'eliah.com';
+// console.log(email);
+
+// let firstName = 'marvel';
+// let lastName = 'okedo';
+// let fullName = firstName + ' ' + lastName;
+// console.log(fullName );
+
+// console.log(fullName.length);
+
+// console.log(fullName.toUpperCase());
+
+// let result = fullName.toLowerCase();
+// console.log(result);
+
+const title = 'Best reads of 2019';
+const author = 'okedo';
+const likes = 30;
+
+// concatenation
+// console.log('The blog called ' +title +' written by ' +author +' having  ' +likes +' likes');
+
+// template string way
+// let result = `The blog called ${title}  written by ${author} having  ${likes} likes`
+// console.log(result);
+
+// Creating html templates
+// let html = `
+// <h2> ${title} </h2>
+// <p> written by ${author} </p>
+// <span> having ${likes} likes </span>
+// `;
+// console.log(html);
+
+// arrey
+let ninjas = ['shaun', 'ryu', 'chun-li'];
+ninjas[1] = 'ken';
+// console.log(ninjas);
+
+let random = ['shaun', 'ryu', 'chun-li', 30, 60];
+// console.log(random.length);
+
+// array methods
+// let result = ninjas.indexOf('chun-li');
+// let result = ninjas.join(', ');
+// let result = ninjas.concat(['ken', 'crystal']);
+// let result = ninjas.push('ken');
+// let result = ninjas.pop();
+
+// console.log(result);
+
+// let age = null;
+// console.log(age, age + 3, `the age is ${age}`);
+
+// boolean
+console.log(true, false, 'true', 'false');
+
+// boolean methods
+let email = 'eliahmwelangi01@gmail.com';
+
+// let results = email.includes('@');
+
+// let results = email.includes('!');
+
+let names = ['stallion', 'okedo', 'sparrow'];
+let results = names.includes('okedo');
+console.log(results);
+
