27e8af9a8ae6944ba6ff9f07f83d6486b1e62c25
okedo <eliahmwelangi01@gmail.com>
2023-09-08 13:57:05 +0300

    Functions

--- node.js
+++ node.js
@@ -360,7 +360,61 @@ const scores = [50, 25, 0, 30, 100, 20, 10];
 
 // arrow functions
 
-const calcArea = radius => 3.14 * radius**2;
+// const calcArea = radius => 3.14 * radius**2;
 
-const area = calcArea(4);
-console.log(area);
+// const area = calcArea(4);
+// console.log(area);
+
+// practice arrow function
+
+// const greet = function(){
+//   return 'hello world!'
+// };
+
+// const greet = () => 'hello world!';
+
+// const result = greet();
+// console.log(result);
+
+// const bill = function (products, tax) {
+//   let total = 0;
+//   for (let i = 0; i < products.length; i++) {
+//     total += products[i] + products[i] * tax;
+//   }
+
+//   return total;
+// }
+// const result = bill([10, 15, 30], 0.2);
+// console.log(result);
+
+
+// arrow function
+
+// const bill = (products, tax) =>  {
+//   let total = 0;
+//   for(let i = 0; i < products.length; i++){
+//     total += products[i] + products[i] * tax;
+//   }
+//   return total;
+// };
+
+// const result = bill([10, 15, 30], 0.2);
+// console.log(result);
+
+// functions and methods
+const name = 'okedo';
+
+// function
+
+const greet = () => 'hello';
+
+let resultOne = greet();
+console.log(resultOne);
+
+// method
+
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);
+
+
+
