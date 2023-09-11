708bd11dfa90c95a01686e8cc64f557f08920a95
okedo <eliahmwelangi01@gmail.com>
2023-09-11 13:54:21 +0300

    random numbers in Math object

--- node.js
+++ node.js
@@ -501,15 +501,18 @@
 
 // Math object
 
-console.log(Math);
-console.log(Math.PI);
+// console.log(Math);
+// console.log(Math.PI);
 
-const area = 9.5;
-console.log(Math.round(area));
+// const area = 9.5;
+// console.log(Math.round(area));
 
-console.log(Math.floor(area));
-console.log(Math.ceil(area));
-console.log(Math.trunc(area));
+// console.log(Math.floor(area));
+// console.log(Math.ceil(area));
+// console.log(Math.trunc(area));
 
-
+// random numbers
+const random = Math.random();
+console.log(random);
+console.log(Math.round(random * 100))
 
