925e4b28252b81c97db09ab0aac5eb30f5614014
okedo <eliahmwelangi01@gmail.com>
2023-09-13 09:42:13 +0300

    DOM click events with event listener

--- node.js
+++ node.js
@@ -694,3 +694,11 @@ const button = document.querySelector('button');
 button.addEventListener('click', () => {
     console.log('you clicked me');
 });
+
+const items = document.querySelectorAll('li');
+
+items.forEach(item => {
+    item.addEventListener('click', () => {
+        console.log('item clicked');
+    });
+});
